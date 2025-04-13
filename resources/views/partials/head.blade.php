99b5e00905a5bdb162894b0c4b366fbef7aa644c
Tony Messias <tonysm@hey.com>
2025-04-13 16:33:07 -0300

    Adds the csrf token meta tag (Turbo will use it automatically)

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport"
     content="width=device-width, initial-scale=1.0{{ $scalable ?? false ? ', maximum-scale=1.0, user-scalable=0' : '' }}" />
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 @if ($transitions ?? false)
     <meta name="view-transition" content="same-origin">
