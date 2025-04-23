e4604a34e9666728d0c3a4e2cf552cd007b272c7
Tony Messias <tonysm@hey.com>
2025-04-23 00:29:23 -0300

    Adds track reload

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -12,6 +12,6 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-<link href="{{ tailwindcss('css/app.css') }}" rel="stylesheet" />
+<link href="{{ tailwindcss('css/app.css') }}" rel="stylesheet" data-turbo-track="reload" />
 
 <x-importmap::tags />
