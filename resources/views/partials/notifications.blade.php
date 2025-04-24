443c2a7324c6e1853182115d1c61638060cbe6fe
Tony Messias <tonysm@hey.com>
2025-04-24 20:26:20 -0300

    switch to inset-x-0 instead of specifying left and right position

--- resources/views/partials/notifications.blade.php
+++ resources/views/partials/notifications.blade.php
@@ -1,4 +1,4 @@
-<div id="notifications" class="fixed top-10 left-0 right-0 flex flex-col items-center justify-center space-y-2 z-10">
+<div id="notifications" class="fixed top-10 inset-x-0 flex flex-col items-center justify-center space-y-2 z-10">
     @if (session()->has('notice'))
         @include('partials.notice', ['message' => session('notice')])
     @endif
