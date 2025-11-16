d481f72745a02bdd1810e79124788d27152c0a65
Tony Messias <tonysm@hey.com>
2025-11-16 19:37:10 -0300

    Fix logo line height

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,5 +3,5 @@
 </div>
 
 <div class="ml-1 grid flex-1 text-left text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">{{ __('Hotwire Starter Kit') }}</span>
+    <span class="mb-0.5 truncate leading-tight font-semibold">{{ __('Hotwire Starter Kit') }}</span>
 </div>
