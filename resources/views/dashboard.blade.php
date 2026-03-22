28564ccddf9b2796c6288dd4be36503f5e3b68ec
Tony Messias <tonysm@hey.com>
2026-03-22 20:46:25 -0300

    Tweaks the dashboard pattern colors

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,21 @@
 <x-layouts.app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div class="relative aspect-video overflow-hidden rounded-box border border-neutral/10 dark:border-neutral/80">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-neutral/30 dark:stroke-neutral/90" />
+            <div class="relative aspect-video overflow-hidden rounded-box border border-base-content/25">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-base-content/25" />
             </div>
 
-            <div class="relative aspect-video overflow-hidden rounded-box border border-neutral/10 dark:border-neutral/80">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-neutral/30 dark:stroke-neutral/90" />
+            <div class="relative aspect-video overflow-hidden rounded-box border border-base-content/25">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-base-content/25" />
             </div>
 
-            <div class="relative aspect-video overflow-hidden rounded-box border border-neutral/10 dark:border-neutral/80">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-neutral/30 dark:stroke-neutral/90" />
+            <div class="relative aspect-video overflow-hidden rounded-box border border-base-content/25">
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-base-content/25" />
             </div>
         </div>
 
-        <div class="relative aspect-video md:aspect-auto h-full min-h-[200px] flex-1 overflow-hidden rounded-box border border-neutral/10 dark:border-neutral/80">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-neutral/30 dark:stroke-neutral/90" />
+        <div class="relative aspect-video md:aspect-auto h-full min-h-[200px] flex-1 overflow-hidden rounded-box border border-base-content/25">
+            <x-placeholder-pattern class="absolute inset-0 size-full stroke-base-content/25" />
         </div>
     </div>
 </x-layouts.app>
