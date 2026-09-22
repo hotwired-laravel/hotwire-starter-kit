b5d4c5fc6ce131818e0bdcbe956a3d3bff8d753a
Tony Messias <tonysm@hey.com>
2026-09-21 23:27:26 -0300

    Replace the custom OTP input with DaisyUI's OTP input

--- resources/views/components/form/otp-input.blade.php
+++ resources/views/components/form/otp-input.blade.php
@@ -2,35 +2,33 @@
     'digits' => 6,
     'name' => 'code',
     'autofocus' => false,
+    'mode' => 'numeric',
 ])
 
-<div
-    data-controller="otp"
-    data-otp-digits-value="@js($digits)"
-    data-action="focus-auth-2fa-auth-code@window->otp#focusFirstInputField clear-auth-2fa-auth-code@window->otp#clearAllInputs"
-    class="relative"
->
-    <div class="flex items-center justify-center space-x-2">
-        @for ($x = 0; $x < $digits; $x++)
-            <input
-                data-otp-target="input"
-                type="text"
-                inputmode="numeric"
-                pattern="[0-9]"
-                maxlength="1"
-                autocomplete="off"
-                @if ($autofocus && $x === 0) autofocus @endif
-                data-action="paste->otp#handlePaste keydown->otp#handleKeydown focus->otp#handleFocus input->otp#sanitizeInput"
-                class="h-10 w-10 input text-center text-sm font-medium @if($x == 0) rounded-l-md @endif @if($x == $digits - 1) rounded-r-md @endif @if($x > 0) -ml-px @endif" />
-        @endfor
-    </div>
+@php
+    $inputmode = match ($mode) {
+        'numeric' => 'numeric',
+        'alphanumeric' => 'text',
+        default => '',
+    };
 
-    <input {{ $attributes->except(['digits'])->merge([
-        'name' => $name,
-        'data-otp-target' => 'code',
-        'type' => 'hidden',
-        'class' => 'hidden',
-        'minlength' => $digits,
-        'maxlength' => $digits,
-    ]) }} />
-</div>
+    $pattern = match ($mode) {
+        'numeric' => sprintf('[0-9]{%d}', $digits),
+        'alphanumeric' => sprintf('[a-zA-Z0-9]{%d}', $digits),
+        default => '',
+    };
+@endphp
+
+<label class="otp">
+    @for ($i = 0; $i < $digits; $i++)<span></span>@endfor
+    <input
+        type="text"
+        {{ $attributes->except(['digits'])->merge([
+            'autocomplete' => "one-time-code",
+            'name' => $name,
+            'inputmode' => $inputmode,
+            'maxlength' => $digits,
+            'pattern' => $pattern,
+        ]) }}
+    />
+</label>
